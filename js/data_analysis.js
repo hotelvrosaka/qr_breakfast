9af364dbcd09825b118d309c3a7c80481151a723
hotelvrosaka <hotelvrosaka@gmail.com>
2025-06-03 11:57:26 +0900

    Update data_analysis.js

--- js/data_analysis.js
+++ js/data_analysis.js
@@ -1,6 +1,6 @@
 function getScriptUrl() {
   // 아래 URL을 배포된 Apps Script 웹 앱 주소로 교체하세요
-  return "https://script.google.com/macros/s/AKfycbyMak4uPVHdcrCxSv-hyzqT4XJVy4IkBWB1rG2-I1Vi_dgATsNGOOKWddqNn37J8DQ71g/exec";
+  return "https://script.google.com/macros/s/AKfycbwRMjixETPUjWHof-vbb4I1s4lf7Cn53HzkmobHgzkgudZrwuNIbMShgrGoDx87OhvDsQ/exec";
 }
 
 // NOTE: 아래 YOUR_DEPLOYED_SCRIPT_ID를 실제 Google Apps Script의 배포 ID로 교체하세요!
